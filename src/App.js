c581602b516c9a551900567587730420b2720477
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-05-16 19:04:08 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+const langs = ["html", "css", "js", "php"];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {langs.map((lang) => {
+          return <li key={lang}>{lang}</li>;
+        })}
+      </ul>
     </div>
   );
 }
